45d63dd6998fe0a7f5d4143e3470e483aef66b06
jason <bjason@gmail.com>
2016-10-02 13:35:32 -0700

    git ignore

---
+++ PriorityApp/ios/Priority/Priority/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Priority
+//
+//  Created by jason on 10/2/16.
+//  Copyright © 2016 jasonify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PriorityApp/ios/Priority/Priority/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Priority
+//
+//  Created by jason on 10/2/16.
+//  Copyright © 2016 jasonify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
